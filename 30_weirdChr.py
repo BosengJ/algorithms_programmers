ff511d462e47ad6c682bd34448215df605a693a2
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-08 14:50:43 +0900

    Update 30_weirdChr.py

--- 30_weirdChr.py
+++ 30_weirdChr.py
@@ -1,21 +1,31 @@
+def changeStr(p):
+    change_str = ""
+    for i in range(len(p)):
+        if i % 2 == 0:
+            new_str = p[i].upper()
+        else:
+            new_str = p[i].lower()
+        change_str += new_str
+    return change_str
+            
+def solution(s):
+    s_list = s.split()
+    answer = ""
+    ans_list = []
+    for p in s_list:
+        new_para = changeStr(p)
+        ans_list.append(new_para)
+    answer = " ".join(ans_list)
+    return answer
 
 s = "try hello world"
+a = 'i love you'
+b = 'i LOvE yOU'
+c = 'I LOVE YOU'
+d = 'i Am jEONg Ga rAm'
 
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lower = "abcdefghijklmnopqrstuvwxyz"
-ind_s = -1
-ans = ""
-
-for chr in s:
-    ind_s = ind_s + 1
-
-    if ind_s  % 2 == 0:
-        ind_l = lower.index(chr)
-        chr_upper = upper[ind_l]
-        ans.   (chr_upper)
-        
-    elif ind_s % 2 == 1:
-        ans.append(chr)
-        
-    else:
-        ans.append(' ')
+print(solution(s))
+print(solution(a))
+print(solution(b))
+print(solution(c))
+print(solution(d))
