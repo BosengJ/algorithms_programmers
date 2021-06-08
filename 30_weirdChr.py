17b8d78e4f9068620a99aa894263d6df741bb0fa
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-08 15:36:59 +0900

    Update 30_weirdChr.py

--- 30_weirdChr.py
+++ 30_weirdChr.py
@@ -1,20 +1,39 @@
-def changeStr(p):
+'''이상한 문자 만들기
+
+문제 설명
+문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 
+각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, 
+solution을 완성하세요.
+
+제한 사항
+문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+
+입출력 예
+s	return
+"try hello world"	"TrY HeLlO WoRlD"
+
+입출력 예 설명
+"try hello world"는 세 단어 "try", "hello", "world"로 구성되어 있습니다. 
+각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 
+"TrY", "HeLlO", "WoRlD"입니다. 따라서 "TrY HeLlO WoRlD" 를 리턴합니다.'''
+
+def changeStr(word):
     change_str = ""
-    for i in range(len(p)):
+    for i in range(len(word)):
         if i % 2 == 0:
-            new_str = p[i].upper()
+            new_str = word[i].upper()
         else:
-            new_str = p[i].lower()
+            new_str = word[i].lower()
         change_str += new_str
     return change_str
             
 def solution(s):
-    s_list = s.split()
+    s_list = s.split(' ')
     answer = ""
     ans_list = []
     for word in s_list:
         new_word = changeStr(word)
         ans_list.append(new_word)
-        print(ans_list)
     answer = " ".join(ans_list)
     return answer
