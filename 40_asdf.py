abb9ff3e6e66f701f9472453641077e653254b70
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-11 07:11:22 +0900

    Update 40_asdf.py

--- 40_asdf.py
+++ 40_asdf.py
@@ -1,13 +1,3 @@
-def sumThreeNum(nums):
-    sum_three_nums = []
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            for k in range(j+1,len(nums)):
-                sum_num = nums[i] + nums[j] + nums[k]
-                sum_three_nums.append(sum_num)
-    sum_three_nums = set(sum_three_nums)
-    return sum_three_nums
-
 def checkPrimeNum(i):
     for j in range(2,i):
         if i % j == 0:
@@ -15,10 +5,16 @@ def checkPrimeNum(i):
     return True
 
 def solution(nums):
-    sum_three_nums = sumThreeNum(nums)
+    sum_three_nums = []
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1,len(nums)):
+                sum_num = nums[i] + nums[j] + nums[k]
+                sum_three_nums.append(sum_num)
+    sum_three_nums = set(sum_three_nums)
+    
     answer = 0
     for i in sum_three_nums:
         if checkPrimeNum(i) == True:
             answer += 1
     return answer
-   
