fb34a99efa1c82ba1d48b9ab2ed6ce0b73c37b72
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-23 15:44:57 +0900

    Update 00_makingfunc.py

--- 00_makingfunc.py
+++ 00_makingfunc.py
@@ -1,8 +1,3 @@
-''' Sort
-Min
-Sum
-Count'''
-
 # Max 값 찾기
 def searchMax(li):
     max_n = 0
@@ -13,10 +8,42 @@ def searchMax(li):
 
 # Min 값 찾기
 def searchMin(li):
-    min_n = 0
     for i in range(len(li)):
-        if i 
+        if i == 0:
+            min_n = li[i]
+        elif li[i] < min_n:
+            min_n = li[i]
+    return min_n
 
+# BubbleSort 오름차순
+def ascending(li):
+    for i in range(len(li)-1):
+        for j in range(len(li)-i-1):
+            if li[j] > li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+    return li
 
-a_list = [1,2,3,4]
-max_n = searchMax(a_list)
+# BubbleSort 내림차순
+def descending(li):
+    for i in range(len(li)-1):
+        for j in range(len(li)-i-1):
+            if li[j] < li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+    return li
+            
+# sum
+def sumList(li):
+    sum_num = 0
+    for num in li:
+        sum_num += num
+    return sum_num
+
+# count
+def countElements(li):
+    dict_li = dict(zip(li,'0'*len(li)))
+    for num in li:
+        for k in dict_li.keys():
+            if num == k:
+                dict_li[k] = int(dict_li[k]) + 1
+    return dict_li
+
